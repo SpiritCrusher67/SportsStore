56a8b2ae9c765a374feabb956f215144214b92a6
sanchez.101 <sanchez.101@mail.ru>
2020-08-26 21:11:17 +0300

    Добавленно разделение на страницы

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,60 @@
+using System;
+using Xunit;
+using Moq;
+using SportsStore.Models;
+using SportsStore.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductId = 1, Name = "P1" } ,
+                new Product { ProductId = 2, Name = "P2"},
+                new Product { ProductId = 3, Name = "P3"},
+                new Product { ProductId = 4, Name = "P4"},
+                new Product { ProductId = 5, Name = "P5"},
+            });
+            var controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            var result = controller.List(2).ViewData.Model as ProductsListViewModel;
+
+            var prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductId = 1, Name = "P1" } ,
+                new Product { ProductId = 2, Name = "P2"},
+                new Product { ProductId = 3, Name = "P3"},
+                new Product { ProductId = 4, Name = "P4"},
+                new Product { ProductId = 5, Name = "P5"},
+            });
+
+            var controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            var result = controller.List(2).ViewData.Model as ProductsListViewModel;
+
+            var pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+    }
+}
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,23 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult List() => View(repository.Products);
-
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public ViewResult List(int page = 1)
+            => View(new ProductsListViewModel{ 
+                Products = repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1)* PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
+           
     }
 }
