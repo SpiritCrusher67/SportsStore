9f60fd263e65f5ec884cd37f6e2dd810a83928c0
sanchez.101 <sanchez.101@mail.ru>
2020-08-26 18:23:59 +0300

    Добавьте файлы проекта.

---
+++ SportsStore/Migrations/20200826151106_Init_data.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class Init_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Category", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Watersports", "A boat for one person", "Kayak", 275m },
+                    { 2, "Watersports", "Protective and fashionable", "LifeJacket", 48.95m },
+                    { 3, "Soccer", "FIFA-approved size and weight", "Soccer Ball", 19.50m },
+                    { 4, "Soccer", "Give your field a proffesional touch", "Corner Flags", 34.95m },
+                    { 5, "Soccer", "Flat-packed 35,000-seat stadium", "Stadium", 79500m },
+                    { 6, "Chess", "Improve brain efficiency by 75%", "Thinking Cap", 16m },
+                    { 7, "Chess", "Secretly give your opponent a disadvantage", "Unsteadly Chair", 29.95m },
+                    { 8, "Chess", "A fun game for the family", "Human Chess Board", 75m },
+                    { 9, "Chess", "Gold-plated, diamond-stuffed King", "Bling-Bling Board", 1200m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 9);
+        }
+    }
+}
---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)  {}
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        ProductId = 1,
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Price = 275,
+                        Category = "Watersports"
+                    },
+                    new Product
+                    {
+                        ProductId = 2,
+                        Name = "LifeJacket",
+                        Description = "Protective and fashionable",
+                        Category = "Watersports",
+                        Price = 48.95m
+                    },
+                    new Product
+                    {
+                        ProductId = 3,
+                        Name = "Soccer Ball",
+                        Description = "FIFA-approved size and weight",
+                        Category = "Soccer",
+                        Price = 19.50m
+                    },
+                    new Product
+                    {
+                        ProductId = 4,
+                        Name = "Corner Flags",
+                        Description = "Give your field a proffesional touch",
+                        Category = "Soccer",
+                        Price = 34.95m
+                    },
+                    new Product
+                    {
+                        ProductId = 5,
+                        Name = "Stadium",
+                        Description = "Flat-packed 35,000-seat stadium",
+                        Category = "Soccer",
+                        Price = 79500
+                    },
+                    new Product
+                    {
+                        ProductId = 6,
+                        Name = "Thinking Cap",
+                        Description = "Improve brain efficiency by 75%",
+                        Category = "Chess",
+                        Price = 16
+                    },
+                    new Product
+                    {
+                        ProductId = 7,
+                        Name = "Unsteadly Chair",
+                        Description = "Secretly give your opponent a disadvantage",
+                        Category = "Chess",
+                        Price = 29.95m
+                    },
+                    new Product
+                    {
+                        ProductId = 8,
+                        Name = "Human Chess Board",
+                        Description = "A fun game for the family",
+                        Category = "Chess",
+                        Price = 75
+                    },
+                    new Product
+                    {
+                        ProductId = 9,
+                        Name = "Bling-Bling Board",
+                        Description = "Gold-plated, diamond-stuffed King",
+                        Category = "Chess",
+                        Price = 1200
+                    }
+                );
+
+        }
+    }
+}
