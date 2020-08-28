9de8274e660601c1161724ea59b1e3d8d550fdb1
sanchez.101 <sanchez.101@mail.ru>
2020-08-28 13:55:59 +0300

    Добавлено отображение списка заказов и возможность отгрузки заказов. Добавлен список товаров

---
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void Index_Contains_All_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name="P1"},
+                new Product {ProductId = 2, Name="P2"},
+                new Product {ProductId = 3, Name="P3"},
+            });
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product[] result
+                = GetViewModel<IEnumerable<Product>>(target.Index())?.ToArray();
+
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+
+}
---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository repository;
+
+        public AdminController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
