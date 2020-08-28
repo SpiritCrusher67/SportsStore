6cfaf0838a2f72a0260acf81cc993c30c63a4a40
sanchez.101 <sanchez.101@mail.ru>
2020-08-28 15:54:27 +0300

    Добавлено создание/обновление/удаление товаров

--- SportsStore.Tests/AdminControllerTests.cs
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Xunit;
 
 namespace SportsStore.Tests
@@ -34,10 +33,99 @@ namespace SportsStore.Tests
             Assert.Equal("P3", result[2].Name);
         }
 
+        [Fact]
+        public void Can_Edit_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name="P1"},
+                new Product {ProductId = 2, Name="P2"},
+                new Product {ProductId = 3, Name="P3"},
+            });
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product p1 = GetViewModel<Product>(target.Edit(1));
+            Product p2 = GetViewModel<Product>(target.Edit(2));
+            Product p3 = GetViewModel<Product>(target.Edit(3));
+
+            Assert.Equal(1, p1.ProductId);
+            Assert.Equal(2, p2.ProductId);
+            Assert.Equal(3, p3.ProductId);
+        }
+
+        [Fact]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name="P1"},
+                new Product {ProductId = 2, Name="P2"},
+                new Product {ProductId = 3, Name="P3"},
+            });
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product result = GetViewModel<Product>(target.Edit(4));
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Can_Save_Valid_Changes()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            Mock<ITempDataDictionary> tempData = new Mock<ITempDataDictionary>();
+
+            var target = new AdminController(mock.Object) { TempData = tempData.Object };
+            Product product = new Product { Name = "Test1" };
+
+            var result = target.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(product));
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", (result as RedirectToActionResult).ActionName);
+        }
+
+        [Fact]
+        public void Cannot_Save_Invalid_Changes()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            var target = new AdminController(mock.Object);
+            Product product = new Product { Name = "Test1" };
+            target.ModelState.AddModelError("error", "error");
+
+            var result = target.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never);
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Can_Delete_Valid_Product()
+        {
+            Product prod = new Product { ProductId = 2, Name = "Test" };
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1"},
+                prod,
+                new Product { ProductId = 3, Name = "P3"}
+            });
+            var target = new AdminController(mock.Object);
+
+            target.Delete(prod.ProductId);
+
+            mock.Verify(m => m.DeleteProduct(prod.ProductId));
+        }
+
         private T GetViewModel<T>(IActionResult result) where T : class
         {
             return (result as ViewResult)?.ViewData.Model as T;
         }
+
     }
 
 }
--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please enter а product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter а description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01,double.MaxValue, ErrorMessage = "Please enter а positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify а category")]
         public string Category { get; set; }
     }
 }
